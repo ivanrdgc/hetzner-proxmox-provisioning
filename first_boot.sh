25e2950694eb5fb1c0423d1204ba49bc9715b70b
Ivan Radigales Creus <ivanrdgc@gmail.com>
2025-10-25 17:42:37 +0200

    Final configuration

--- first_boot.sh
+++ first_boot.sh
@@ -106,6 +106,11 @@ dhcp-rapid-commit
 dhcp-range=10.0.0.100,10.0.255.254,255.255.0.0,12h
 dhcp-option=3,10.0.0.1
 dhcp-option=6,1.1.1.1,8.8.8.8
+
+# IPv6 SLAAC + RA
+enable-ra
+dhcp-range=::100,::ffff,constructor:vmbr0,ra-stateless,ra-names,12h
+dhcp-option=option6:dns-server,[2606:4700:4700::1111],[2001:4860:4860::8888]
 EOF
 
 systemctl restart dnsmasq
